bf29d1d06218b236e714aeb8d710f6d5e51f4978
generic-name-2166 <kirbesson277@mail.ru>
2025-01-11 22:09:28 +0300

    fix(back): swc doesnt like * as

--- blogs-back-end/src/auth.ts
+++ blogs-back-end/src/auth.ts
@@ -1,6 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import * as bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
 
 // using bcryptjs because bcrypt's dependencies don't play well with esbuild
 
